e65686020c5c0ac888df3ce6eed79d4afe8d267c
Raouf Fathi <abdulraoufswehli58@gmail.com>
2019-04-25 03:16:31 +0800

    Updating Resuem URL

--- src/components/bio.js
+++ src/components/bio.js
@@ -43,7 +43,7 @@ const Bio = ({location}) => (
             </div>
 
             <div className="btn-wrapper mt-5  ">
-            <a href="https://s3.amazonaws.com/raoufrey/CV.pdf" class="btn btn-warning btn-icon" aria-label="CV">
+            <a href="https://s3-ap-southeast-1.amazonaws.com/raouf.de/CV.pdf" class="btn btn-warning btn-icon" aria-label="CV">
              
                   <i class="fa fa-cloud-download mr-2    "></i>
                
